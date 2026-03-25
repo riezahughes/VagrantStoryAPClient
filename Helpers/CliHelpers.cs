cee6887b4b0e88f390cddf31df8f5188d4434772
riezahughes <dan@tantalus-troupe.com>
2026-03-25 13:15:31 +0100

    cli comands fixes

--- Helpers/CliHelpers.cs
+++ Helpers/CliHelpers.cs
@@ -64,18 +64,19 @@ namespace VagrantStoryArchipelago.Helpers
         public static void RunOptions(ArchipelagoClient client)
         {
             PlayerVictoryConditions currentGoal = PlayerStateHelpers.GetPlayerOption<PlayerVictoryConditions>(client.Options, "goal");
-            PlayerStartingPosition openWorldOption = PlayerStateHelpers.GetPlayerOption<PlayerStartingPosition>(client.Options, "starting_position");
+            ProgressionOptions progressionOption = PlayerStateHelpers.GetPlayerOption<ProgressionOptions>(client.Options, "progression_option");
+            PlayerStartingPosition openWorldStart = PlayerStateHelpers.GetPlayerOption<PlayerStartingPosition>(client.Options, "open_world_option");
             ItemPoolDropOptions itemDropOption = PlayerStateHelpers.GetPlayerOption<ItemPoolDropOptions>(client.Options, "item_drop");
-            DropItemChoice chestItemChoices = PlayerStateHelpers.GetPlayerOption<DropItemChoice>(client.Options, "chest_items");
-            DropItemChoice bossItemChoices = PlayerStateHelpers.GetPlayerOption<DropItemChoice>(client.Options, "boss_items");
-            SkillUnlockOptions chainSkillSetting = PlayerStateHelpers.GetPlayerOption<SkillUnlockOptions>(client.Options, "chain_skill");
-            SkillUnlockOptions breakArtSetting = PlayerStateHelpers.GetPlayerOption<SkillUnlockOptions>(client.Options, "break_art");
-            PoolItemOptions newGamePlus = PlayerStateHelpers.GetPlayerOption<PoolItemOptions>(client.Options, "new_game_plus");
+            DropItemChoice chestItemChoices = PlayerStateHelpers.GetPlayerOption<DropItemChoice>(client.Options, "chest_item_choices");
+            DropItemChoice bossItemChoices = PlayerStateHelpers.GetPlayerOption<DropItemChoice>(client.Options, "boss_item_choices");
+            SkillUnlockOptions chainSkillSetting = PlayerStateHelpers.GetPlayerOption<SkillUnlockOptions>(client.Options, "chain_skill_unlock_option");
+            SkillUnlockOptions breakArtSetting = PlayerStateHelpers.GetPlayerOption<SkillUnlockOptions>(client.Options, "break_art_unlock_option");
+            PoolItemOptions newGamePlus = PlayerStateHelpers.GetPlayerOption<PoolItemOptions>(client.Options, "include_new_game_plus");
             PoolItemOptions includeTeleport = PlayerStateHelpers.GetPlayerOption<PoolItemOptions>(client.Options, "include_teleport");
             OptionToggle zeroMpTeleport = PlayerStateHelpers.GetPlayerOption<OptionToggle>(client.Options, "zero_mp_teleport");
             OptionToggle openTeleport = PlayerStateHelpers.GetPlayerOption<OptionToggle>(client.Options, "open_teleport");
-            OptionToggle roomSanity = PlayerStateHelpers.GetPlayerOption<OptionToggle>(client.Options, "room_sanity");
-            OptionToggle panelSanity = PlayerStateHelpers.GetPlayerOption<OptionToggle>(client.Options, "panel_sanity");
+            OptionToggle roomSanity = PlayerStateHelpers.GetPlayerOption<OptionToggle>(client.Options, "roomsanity");
+            OptionToggle panelSanity = PlayerStateHelpers.GetPlayerOption<OptionToggle>(client.Options, "panelsanity");
             OptionToggle deathlink = PlayerStateHelpers.GetPlayerOption<OptionToggle>(client.Options, "deathlink");
 
             int breakArtCounter = PlayerStateHelpers.GetPlayerOptionCounts(client.Options, "break_art_counter");
@@ -83,9 +84,12 @@ namespace VagrantStoryArchipelago.Helpers
             int heavyDropLimit = PlayerStateHelpers.GetPlayerOptionCounts(client.Options, "heavy_drop_limit");
             int guaranteedItems = PlayerStateHelpers.GetPlayerOptionCounts(client.Options, "guaranteed_items");
 
+            string progwrite = progressionOption == ProgressionOptions.OPEN ? openWorldStart.ToString() : "N/A";
+
             Console.WriteLine($"--- RUN OPTIONS ---");
             Console.WriteLine($"Goal: {currentGoal}");
-            Console.WriteLine($"Progression: {openWorldOption}");
+            Console.WriteLine($"Progression: {progressionOption}");
+            Console.WriteLine($"Open World Start: {progwrite}");
             Console.WriteLine($"Item Drop Option: {itemDropOption}");
             Console.WriteLine($"Chest Item Choices: {chestItemChoices}");
             Console.WriteLine($"Boss Item Choices: {bossItemChoices}");
@@ -109,44 +113,44 @@ namespace VagrantStoryArchipelago.Helpers
         {
 
             ReadOnlyCollection<ItemInfo> items = client.CurrentSession.Items.AllItemsReceived;
-
+            int index = App.ProcessedItemIndex - 1;
 
             Console.WriteLine($"--- RUN STATUS ---");
-            Console.WriteLine($"Current Items Collected: {App.ProcessedItemIndex}");
-            Console.WriteLine($"Last Item: {items[App.ProcessedItemIndex]}");
+            Console.WriteLine($"Current Items Collected: {index}");
+            Console.WriteLine($"Last Item: {items[index]}");
             Console.WriteLine($"--- KEY ITEM CHECK ---");
-            Console.WriteLine($"Bronze Key: {items.Any(i => i.ItemName == "Bronze Key") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Bronze Key").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Iron Key: {items.Any(i => i.ItemName == "Iron Key") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Iron Key").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Silver Key: {items.Any(i => i.ItemName == "Silver Key") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Silver Key").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Gold Key: {items.Any(i => i.ItemName == "Gold Key") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Gold Key").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Platinum Key: {items.Any(i => i.ItemName == "Platinum Key") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Platinum Key").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Steel Key: {items.Any(i => i.ItemName == "Steel Key") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Steel Key").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Crimson Key: {items.Any(i => i.ItemName == "Crimson Key") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Crimson Key").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Chest Key: {items.Any(i => i.ItemName == "Chest Key") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Chest Key").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Chamomile Sigil: {items.Any(i => i.ItemName == "Chamomile Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Chamomile Sigil").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Lily Sigil: {items.Any(i => i.ItemName == "Lily Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Lily Sigil").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Tearose Sigil: {items.Any(i => i.ItemName == "Tearose Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Tearose Sigil").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Clematis Sigil: {items.Any(i => i.ItemName == "Clematis Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Clematis Sigil").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Hyacinth Sigil: {items.Any(i => i.ItemName == "Hyacinth Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Hyacinth Sigil").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Fern Sigil: {items.Any(i => i.ItemName == "Fern Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Fern Sigil").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Aster Sigil: {items.Any(i => i.ItemName == "Aster Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Aster Sigil").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Eulelia Sigil: {items.Any(i => i.ItemName == "Eulelia Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Eulelia Sigil").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Melissa Sigil: {items.Any(i => i.ItemName == "Melissa Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Melissa Sigil").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Calla Sigil: {items.Any(i => i.ItemName == "Calla Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Calla Sigil").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Laurel Sigil: {items.Any(i => i.ItemName == "Laurel Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Laurel Sigil").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Acacia Sigil: {items.Any(i => i.ItemName == "Acacia Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Acacia Sigil").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Palm Sigil: {items.Any(i => i.ItemName == "Palm Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Palm Sigil").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Kalmia Sigil: {items.Any(i => i.ItemName == "Kalmia Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Kalmia Sigil").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Colombine Sigil: {items.Any(i => i.ItemName == "Colombine Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Colombine Sigil").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Anemone Sigil: {items.Any(i => i.ItemName == "Anemone Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Anemone Sigil").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Verbena Sigil: {items.Any(i => i.ItemName == "Verbena Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Verbena Sigil").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Schirra Sigil: {items.Any(i => i.ItemName == "Schirra Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Schirra Sigil").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Marigold Sigil: {items.Any(i => i.ItemName == "Marigold Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Marigold Sigil").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Azalea Sigil: {items.Any(i => i.ItemName == "Azalea Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Azalea Sigil").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Tigertail Sigil: {items.Any(i => i.ItemName == "Tigertail Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Tigertail Sigil").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Stock Sigil: {items.Any(i => i.ItemName == "Stock Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Stock Sigil").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Cattleya Sigil: {items.Any(i => i.ItemName == "Cattleya Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Cattleya Sigil").index <= App.ProcessedItemIndex}");
-            Console.WriteLine($"Mandrake Sigil: {items.Any(i => i.ItemName == "Mandrake Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Mandrake Sigil").index <= App.ProcessedItemIndex}");
+            Console.WriteLine($"Bronze Key: {items.Any(i => i.ItemName == "Bronze Key") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Bronze Key").index <= index}");
+            Console.WriteLine($"Iron Key: {items.Any(i => i.ItemName == "Iron Key") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Iron Key").index <= index}");
+            Console.WriteLine($"Silver Key: {items.Any(i => i.ItemName == "Silver Key") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Silver Key").index <= index}");
+            Console.WriteLine($"Gold Key: {items.Any(i => i.ItemName == "Gold Key") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Gold Key").index <= index}");
+            Console.WriteLine($"Platinum Key: {items.Any(i => i.ItemName == "Platinum Key") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Platinum Key").index <= index}");
+            Console.WriteLine($"Steel Key: {items.Any(i => i.ItemName == "Steel Key") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Steel Key").index <= index}");
+            Console.WriteLine($"Crimson Key: {items.Any(i => i.ItemName == "Crimson Key") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Crimson Key").index <= index}");
+            Console.WriteLine($"Chest Key: {items.Any(i => i.ItemName == "Chest Key") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Chest Key").index <= index}");
+            Console.WriteLine($"Chamomile Sigil: {items.Any(i => i.ItemName == "Chamomile Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Chamomile Sigil").index <= index}");
+            Console.WriteLine($"Lily Sigil: {items.Any(i => i.ItemName == "Lily Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Lily Sigil").index <= index}");
+            Console.WriteLine($"Tearose Sigil: {items.Any(i => i.ItemName == "Tearose Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Tearose Sigil").index <= index}");
+            Console.WriteLine($"Clematis Sigil: {items.Any(i => i.ItemName == "Clematis Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Clematis Sigil").index <= index}");
+            Console.WriteLine($"Hyacinth Sigil: {items.Any(i => i.ItemName == "Hyacinth Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Hyacinth Sigil").index <= index}");
+            Console.WriteLine($"Fern Sigil: {items.Any(i => i.ItemName == "Fern Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Fern Sigil").index <= index}");
+            Console.WriteLine($"Aster Sigil: {items.Any(i => i.ItemName == "Aster Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Aster Sigil").index <= index}");
+            Console.WriteLine($"Eulelia Sigil: {items.Any(i => i.ItemName == "Eulelia Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Eulelia Sigil").index <= index}");
+            Console.WriteLine($"Melissa Sigil: {items.Any(i => i.ItemName == "Melissa Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Melissa Sigil").index <= index}");
+            Console.WriteLine($"Calla Sigil: {items.Any(i => i.ItemName == "Calla Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Calla Sigil").index <= index}");
+            Console.WriteLine($"Laurel Sigil: {items.Any(i => i.ItemName == "Laurel Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Laurel Sigil").index <= index}");
+            Console.WriteLine($"Acacia Sigil: {items.Any(i => i.ItemName == "Acacia Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Acacia Sigil").index <= index}");
+            Console.WriteLine($"Palm Sigil: {items.Any(i => i.ItemName == "Palm Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Palm Sigil").index <= index}");
+            Console.WriteLine($"Kalmia Sigil: {items.Any(i => i.ItemName == "Kalmia Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Kalmia Sigil").index <= index}");
+            Console.WriteLine($"Colombine Sigil: {items.Any(i => i.ItemName == "Colombine Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Colombine Sigil").index <= index}");
+            Console.WriteLine($"Anemone Sigil: {items.Any(i => i.ItemName == "Anemone Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Anemone Sigil").index <= index}");
+            Console.WriteLine($"Verbena Sigil: {items.Any(i => i.ItemName == "Verbena Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Verbena Sigil").index <= index}");
+            Console.WriteLine($"Schirra Sigil: {items.Any(i => i.ItemName == "Schirra Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Schirra Sigil").index <= index}");
+            Console.WriteLine($"Marigold Sigil: {items.Any(i => i.ItemName == "Marigold Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Marigold Sigil").index <= index}");
+            Console.WriteLine($"Azalea Sigil: {items.Any(i => i.ItemName == "Azalea Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Azalea Sigil").index <= index}");
+            Console.WriteLine($"Tigertail Sigil: {items.Any(i => i.ItemName == "Tigertail Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Tigertail Sigil").index <= index}");
+            Console.WriteLine($"Stock Sigil: {items.Any(i => i.ItemName == "Stock Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Stock Sigil").index <= index}");
+            Console.WriteLine($"Cattleya Sigil: {items.Any(i => i.ItemName == "Cattleya Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Cattleya Sigil").index <= index}");
+            Console.WriteLine($"Mandrake Sigil: {items.Any(i => i.ItemName == "Mandrake Sigil") && items.Select((item, index) => new { item, index }).First(x => x.item.ItemName == "Mandrake Sigil").index <= index}");
             Console.WriteLine($"------------------");
         }
 
